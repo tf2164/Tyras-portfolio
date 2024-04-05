4737d974c567a24b07e4d3ff982d877100e89a5c
tf2164 <tyraafair@gmail.com>
2024-04-04 20:29:44 -0400

    temporary comments

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-
 import React, { useState } from "react";
 import About from "./components/About.js";
 import Contact from "./components/Contact.js";
 import Navbar from "./components/Navbar.js";
 import Projects from "./components/Projects.js";
 import Skills from './components/Skills.js';
-import { DarkModeProvider } from './Dark mode/DarkModeContext.js'
+import { DarkModeProvider } from './Dark mode/DarkModeContext.js';
 import DarkModeToggle from './Dark mode/DarkModeToggle';
 import Chatbot from "react-chatbot-kit";
 import ActionProvider from "./components/chatbot/ActionProvider";
 import MessageParser from "./components/chatbot/MessageParser";
 import config from "./components/chatbot/config";
-import user from "./components/assets/user.png"
-import chat from "./components/assets/chat.png"
+import user from "./components/assets/user.png";
+import chat from "./components/assets/chat.png";
+import '../src/index.css';
 
 function App() {
   const [activeSection, setActiveSection] = useState('section1');
   const [isChatbotOpen, setIsChatbotOpen] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   const handleNavigation = (sectionId) => {
     setActiveSection(sectionId);
@@ -28,39 +29,50 @@ function App() {
     setIsChatbotOpen(!isChatbotOpen);
   };
 
+  const toggleSidebar = () => {
+    console.log("Toggling sidebar");
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+  
+
   return (
     <DarkModeProvider>
       <main className="main-container">
         <div id="combine-slider">
-          <Navbar /><DarkModeToggle />
+          <Navbar />
+          <DarkModeToggle />
         </div>
 
-        <div>
-          <div className="sidebar">
-            <ul style={{ listStyleType: 'none' }}>
-              <li onClick={() => handleNavigation('section1')}>About</li>
-              <li onClick={() => handleNavigation('section2')}>Projects</li>
-              <li onClick={() => handleNavigation('section3')}>Skills</li>
-              <li onClick={() => handleNavigation('section4')}>Work with me</li>
-            </ul>
-          </div>
+        <div className={`navside ${isSidebarOpen ? 'visible' : 'hidden'}`}>
+        <div className="sidebar">
+          <ul style={{ listStyleType: 'none' }}>
+            <li onClick={() => handleNavigation('section1')}>About</li>
+            <li onClick={() => handleNavigation('section2')}>Projects</li>
+            <li onClick={() => handleNavigation('section3')}>Skills</li>
+            <li onClick={() => handleNavigation('section4')}>Work with me</li>
+          </ul>
+        </div>
+      </div>
+      <button className="sidebar-toggle-btn" onClick={toggleSidebar}>
+        {isSidebarOpen ? 'Close Sidebar' : 'Open Sidebar'}
+      </button>
+  
 
           <div className="content">
             <div id="section1" className={activeSection === 'section1' ? 'active' : 'hidden'}>
-              {<About />}
+              <About />
             </div>
             <div id="section2" className={activeSection === 'section2' ? 'active' : 'hidden'}>
-              {/* Content for Section 2 */}
+              <Projects />
             </div>
             <div id="section3" className={activeSection === 'section3' ? 'active' : 'hidden'}>
-              {<Projects />}
             </div>
             <div id="section4" className={activeSection === 'section4' ? 'active' : 'hidden'}>
-              {<Skills />}
-              {<Contact />}
+              <Skills />
+              <Contact />
             </div>
           </div>
-        </div>
+       
 
         <aside className='chatbot-positioning'>
           {isChatbotOpen && (
@@ -69,10 +81,11 @@ function App() {
               actionProvider={ActionProvider}
               messageParser={MessageParser}
               profileAvatar={user}
-
             />
           )}
-          <button className="chat-toggle" onClick={toggleChatbot}> <img src={chat} alt="chat-icon" /></button>
+          <button className="chat-toggle" onClick={toggleChatbot}>
+            <img src={chat} alt="chat-icon" />
+          </button>
         </aside>
       </main>
     </DarkModeProvider>
@@ -80,7 +93,6 @@ function App() {
 }
 
 export default App;
-
 
 
 // import './App.css';
--- src/components/data.js
+++ src/components/data.js
@@ -15,30 +15,30 @@ import todo from './assets/project-imgs/to-do-imh.png'
 
 
 export const projects = [
-    {
-      title: "React Reserve",
-      subtitle: "MERN Stack",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-1.gif",
-      link: "https://reactbootcamp.com",
-    },
-    {
-      title: "React Tracks",
-      subtitle: "React and Python",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-2.gif",
-      link: "https://reedbarger.com",
-    },
-    {
-      title: "DevChat",
-      subtitle: "React and Firebase",
-      description:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-      image: "./project-3.gif",
-      link: "https://jsbootcamp.com",
-    },
+    // {
+    //   title: "React Reserve",
+    //   subtitle: "MERN Stack",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    //   image: "./project-1.gif",
+    //   link: "",
+    // },
+    // {
+    //   title: "React Tracks",
+    //   subtitle: "React and Python",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    //   image: "./project-2.gif",
+    //   link: "",
+    // },
+    // {
+    //   title: "DevChat",
+    //   subtitle: "React and Firebase",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    //   image: "./project-3.gif",
+    //   link: "",
+    // },
     {
       title: "List Track",
       subtitle: "Javascript DOM",
