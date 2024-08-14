16604de1f0ba00b043adf18815df1ceecede6f64
tf2164 <tyraafair@gmail.com>
2024-08-14 15:06:35 -0400

    cleaned css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,15 +9,16 @@ export default function Projects() {
   return (
     <section id="projects">
       <div className="">
+        <div className="project-block">
         <div className="section-header">
           <h1 className="section-title">
-            Apps I've Built test
+            Apps I've Built
           </h1>
           <p className="section-description">
           These are all projects built outside of academic responsibilities. I recreated a couple of projects from my apprenticeship with new skills.
            </p>
         </div>
-        <div className="project-block">
+
           {projects.map((project) => (
             <a
               href={project.link}
