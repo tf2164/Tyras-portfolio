1f9861c114fdbfca5642aaf343c6a08426edb909
tf2164 <tyraafair@gmail.com>
2024-12-10 09:16:51 -0500

    about

--- src/components/About.js
+++ src/components/About.js
@@ -14,8 +14,15 @@ export default function About() {
             apps.
           </h1>
           <p className="caption-hero">
-          I am a passionate and dedicated Software Engineer with significant hands-on experience gained through an impactful apprenticeship and a data-focused role at Capital One. 
-          My apprenticeship at Road to Hire provided me with a strong foundation in full stack development through six months of intensive, hands-on projects. This blend of practical experience and my upcoming B.S. in Software Engineering from Western Governors University, sets me apart from candidates who have only academic experience. My diverse skill set and proven ability to apply knowledge in real-world settings make me a strong fit for an entry-level software engineering role, where I can contribute effectively and grow within the team.
+          I am a passionate and dedicated Software Engineer with significant hands-on experience gained through an impactful apprenticeship 
+          and a data-focused role at Capital One. 
+          My apprenticeship at Road to Hire provided me with a strong foundation in 
+          full stack development through six months of intensive, hands-on projects. 
+          This blend of practical experience and my educational pursuits, sets me apart from candidates who have 
+           only academic experience. My diverse skill set and proven ability to apply
+            knowledge in real-world settings make me a strong fit for an entry-level 
+            software engineering role, where I can contribute effectively and grow
+             within the team.
           </p>
           <br></br>
 
